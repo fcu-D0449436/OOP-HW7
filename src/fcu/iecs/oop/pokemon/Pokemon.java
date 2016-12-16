8b626e6aeb9994bb2b7691b80deef7438d5a52fa
user <user@FH12>
2016-12-16 09:50:25 +0800

    OOP-HW7

---
+++ src/fcu/iecs/oop/pokemon/Pokemon.java
@@ -0,0 +1,30 @@
+package fcu.iecs.oop.pokemon;
+
+public abstract class Pokemon {
+
+	
+	private final String name;
+	private final PokemonType type;
+    private int cp;
+    
+	public Pokemon(String name,PokemonType type,int cp){
+		this.name=name;
+		this.type=type;
+		this.cp=cp;
+	}
+	public String getName() {
+	    return name;
+	}
+	public int getCP(){
+		return cp;
+	}
+	public PokemonType getType(){
+		return type;
+	}
+	public void setCP(int cp) {
+		this.cp = cp;
+	}
+
+	public abstract void attack();
+	
+}
